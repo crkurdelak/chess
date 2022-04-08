8409349002b7ce4bfe773a0a3fac9db0aceb9965
crkur <ckurdelak20@georgefox.edu>
2022-04-08 09:18:30 -0700

    spooky comment skeletons

--- Board.cpp
+++ Board.cpp
@@ -1,2 +1,30 @@
 #include "Board.h"
 #include "Piece.h"
+
+Board::Board() {
+
+}
+
+Square &Board::square_at(size_t rank, size_t file) const {
+    return <#initializer#>;
+}
+
+bool Board::is_clear_rank(const Square &from, const Square &to) const {
+    return false;
+}
+
+bool Board::is_clear_file(const Square &from, const Square &to) const {
+    return false;
+}
+
+bool Board::is_clear_diag(const Square &from, const Square &to) const {
+    return false;
+}
+
+Board::~Board() {
+
+}
+
+std::ostream &operator<<(std::ostream &os, const Board &board) {
+    return <#initializer#>;
+}
--- Board.h
+++ Board.h
@@ -8,20 +8,69 @@ class Board {
   public:
     static const size_t SIZE = 8;
 
+
+    /**
+     * @brief
+     *
+     * Allocate squares
+     */
     Board();
 
+
+    /**
+     * @brief
+     * @param rank
+     * @param file
+     * @return
+     */
     Square& square_at(size_t rank, size_t file) const;
 
+
+    /**
+     * @brief Returns true if the given rank is clear.
+     * @param from
+     * @param to
+     * @return true if the given rank is a valid rank and is clear; else return false
+     */
     bool is_clear_rank(const Square& from, const Square& to) const;
+
+
+    /**
+     * @brief
+     * @param from
+     * @param to
+     * @return
+     */
     bool is_clear_file(const Square& from, const Square& to) const;
+
+
+    /**
+     *
+     * @param from
+     * @param to
+     * @return
+     */
     bool is_clear_diag(const Square& from, const Square& to) const;
 
+
+    /**
+     *
+     *
+     * Delete all squares you allocated
+     */
     ~Board();
 
   private:
     Square* _squares[SIZE][SIZE] = {};
 };
 
+
+/**
+ * Sends the board to an output stream.
+ * @param os
+ * @param board
+ * @return
+ */
 std::ostream& operator<<(std::ostream& os, const Board& board);
 
 #endif
