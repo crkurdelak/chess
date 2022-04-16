50b9d378a0452b6669c51f1acd29feac5c76471a
crkur <ckurdelak20@georgefox.edu>
2022-04-15 22:09:01 -0700

    https://static.wikia.nocookie.net/deltarune/images/f/f0/Queen_face_lmao.png/revision/latest/scale-to-width-down/76?cb=20211002161612

--- Queen.cpp
+++ Queen.cpp
@@ -1,1 +1,33 @@
 #include "Queen.h"
+#include "Square.h"
+
+piece_value_t Queen::value() const {
+    return 9;
+}
+
+bool Queen::can_move_to(const Square &location) const {
+    const double EPSILON = 1e-10;
+    bool result = false;
+    double slope = (location.rank() * 1.0 - this -> location()->rank()) / (location.file() * 1.0 -
+            this -> location()->file());
+
+    if (&location != this -> location()
+    && (slope - 1 <= EPSILON
+    || slope + 1 <= EPSILON
+    || location.file() == this -> location()->file()
+    ||location.rank() == this -> location()->rank())) {
+        result = true;
+    }
+    return result;
+}
+
+std::string Queen::str() const {
+    std::string piece_symbol;
+    if (this -> color() == Piece::Color::black) {
+        piece_symbol = "♛";
+    }
+    else {
+        piece_symbol = "♕";
+    }
+    return piece_symbol;
+}
--- Queen.h
+++ Queen.h
@@ -6,12 +6,32 @@
 
 class Queen : public Piece {
   public:
+    /**
+     * @brief Creates a new queen.
+     * @param color the color of the queen
+     * @param location the location of the queen
+     */
     Queen(Piece::Color color, Square& location) : Piece(color, location) { }
 
+
+    /**
+     *
+     * @copydoc Piece::value
+     */
     piece_value_t value() const override;
 
+
+    /**
+     *
+     * @copydoc Piece::can_move_to
+     */
     bool can_move_to(const Square& location) const override;
 
+
+    /**
+     *
+     * @copydoc Piece::str
+     */
     std::string str() const override;
 };
 
